7650333cac9d27f3b36ce3de5d55f019948a0bec
duolok <dusan.lecic5@gmail.com>
2026-02-26 00:30:14 +0100

    add: internal errors for gateway
    This adds internal error management. Error contains a code, message,
    details about it (like the resource or the name/id) and cause which is
    another error. This file is supposed to be a tiny internal wrapper.

---
+++ gateway/internal/errors/errors.go
@@ -0,0 +1,82 @@
+package errors
+
+import (
+	"fmt"
+)
+
+type Code string
+
+const (
+	ErrNotFound         Code = "NOT_FOUND"
+	ErrAlreadyExists    Code = "ALREADY_EXISTS"
+	ErrInvalidInput     Code = "INVALID_INPUT"
+	ErrValidationFailed Code = "VALIDATION_FAILED"
+	ErrBuildFailed      Code = "BUILD_FAILED"
+	ErrDeployFailed     Code = "DEPLOY_FAILED"
+	ErrUpstreamTimeout  Code = "UPSTREAM_TIMEOUT"
+	ErrInternal         Code = "INTERNAL"
+)
+
+type AppError struct {
+	Code    Code                   `json:"code"`
+	Message string                 `json:"message"`
+	Details map[string]interface{} `json:"details,omitempty"`
+	Cause   error                  `json:"-"`
+}
+
+func (e *AppError) Error() string {
+	if e.Cause != nil {
+		return fmt.Sprintf("[%s] %s: %v", e.Code, e.Message, e.Cause)
+	}
+	return fmt.Sprintf("[%s] %s", e.Code, e.Message)
+}
+
+func (e *AppError) Unwrap() error {
+	return e.Cause
+}
+
+func NotFound(resource string, id interface{}) *AppError {
+	return &AppError{
+		Code:    ErrNotFound,
+		Message: fmt.Sprintf("%s not found", resource),
+		Details: map[string]interface{}{"resource": resource, "id": id},
+	}
+}
+
+func AlreadyExists(resource, name string) *AppError {
+	return &AppError{
+		Code:    ErrAlreadyExists,
+		Message: fmt.Sprintf("%s '%s' already exists", resource, name),
+		Details: map[string]interface{}{"resource": resource, "name": name},
+	}
+}
+
+func InvalidInput(message string) *AppError {
+	return &AppError{
+		Code:    ErrInvalidInput,
+		Message: message,
+	}
+}
+
+func Internal(message string, cause error) *AppError {
+	return &AppError{
+		Code:    ErrInternal,
+		Message: message,
+		Cause:   cause,
+	}
+}
+
+func StatusCode(err *AppError) int {
+	switch err.Code {
+	case ErrNotFound:
+		return 404
+	case ErrAlreadyExists:
+		return 409
+	case ErrInvalidInput, ErrValidationFailed:
+		return 400
+	case ErrUpstreamTimeout:
+		return 504
+	default:
+		return 500
+	}
+}
