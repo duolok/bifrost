054ec0cd80a994247490d2499197276244a96c10
duolok <dusan.lecic5@gmail.com>
2026-02-25 23:56:55 +0100

    add: project struct
    Project struct defines all the information about the project that is
    being deployed. Name explains it.
    
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

---
+++ gateway/internal/models/project.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Project struct {
+	ID            uuid.UUID        `json:"id"`
+	Name          string           `json:"name"`
+	RepoURL       string           `json:"repo_url"`
+	DefaultBranch string           `json:"default_branch"`
+	WebhookSecret string           `json:"-"`
+	Config        *json.RawMessage `json:"config,omitempty"`
+	Status        string           `json:"status"`
+	CreatedAt     time.Time        `json:"created_at"`
+	UpdatedAt     time.Time        `json:"updated_at"`
+}
+
+type CreateProjectRequest struct {
+	Name          string `json:"name" binding:"required,min=1,max=255"`
+	RepoURL       string `json:"repo_url" binding:"required,url"`
+	DefaultBranch string `json:"default_branch"`
+}
