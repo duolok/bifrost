bc02ba056d675b2b73e50ef27d159a30cf7a0cb3
duolok <dusan.lecic5@gmail.com>
2026-03-09 20:57:04 +0100

    add: deployment const
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- gateway/internal/api/consts.go
+++ gateway/internal/api/consts.go
@@ -4,13 +4,15 @@ const (
 	resourceProject    = "project"
 	resourceDeployment = "deployment"
 
-	auditProjectCreated     = "project.created"
-	auditProjectDeleted     = "project.deleted"
-	auditDeployTriggered    = "deployment.triggered"
-	auditWebhookReceived    = "webhook.received"
+	auditProjectCreated  = "project.created"
+	auditProjectDeleted  = "project.deleted"
+	auditDeployTriggered = "deployment.triggered"
+	auditWebhookReceived = "webhook.received"
+	auditDeployComplete  = "deployment.deployed"
+	auditDeployFailed    = "deployment.failed"
 
-	triggerAPI            = "api"
-	triggerGitHubWebhook  = "github-webhook"
+	triggerAPI           = "api"
+	triggerGitHubWebhook = "github-webhook"
 
 	pgUniqueViolation = "23505"
 )
