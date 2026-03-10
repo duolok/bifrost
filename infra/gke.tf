38bc3954deb997502c46f8e3bb719adc330ec4f1
duolok <dusan.lecic5@gmail.com>
2026-03-10 07:58:09 +0100

    add: terraform kubernetes
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

---
+++ infra/gke.tf
@@ -0,0 +1,47 @@
+resource "google_compute_network" "vpc" {
+  name                    = "bifrost-vpc"
+  auto_create_subnetworks = false
+
+  depends_on = [google_project_service.apis]
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "bifrost-subnet"
+  ip_cidr_range = "10.0.0.0/20"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "10.4.0.0/14"
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "10.8.0.0/20"
+  }
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "bifrost-cluster"
+  location = var.region
+
+  enable_autopilot = true
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+
+  ip_allocation_policy {}
+
+  depends_on = [google_project_service.apis]
+}
