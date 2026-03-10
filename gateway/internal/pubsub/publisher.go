46006dcd1da661bc92d2e187cbec2c45aef93301
duolok <dusan.lecic5@gmail.com>
2026-03-10 20:01:20 +0100

    add: google publisher
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

---
+++ gateway/internal/pubsub/publisher.go
@@ -0,0 +1,63 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"strings"
+
+	gcppubsub "cloud.google.com/go/pubsub"
+)
+
+type BuildRequest struct {
+	DeployID    string `json:"deploy_id"`
+	ProjectName string `json:"project_name"`
+	RepoURL     string `json:"repo_url"`
+	CommitSHA   string `json:"commit_sha"`
+	ImageURI    string `json:"image_uri"`
+}
+
+type Publisher struct {
+	topic  *gcppubsub.Topic
+	arRepo string
+}
+
+func NewPublisher(ctx context.Context, gcpProject, topicName, arRepo string) (*Publisher, error) {
+	client, err := gcppubsub.NewClient(ctx, gcpProject)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Publisher{
+		topic:  client.Topic(topicName),
+		arRepo: arRepo,
+	}, nil
+}
+
+func (p *Publisher) PublishBuildRequest(ctx context.Context, req BuildRequest) error {
+	data, err := json.Marshal(req)
+	if err != nil {
+		return fmt.Errorf("marshal build request: %w", err)
+	}
+
+	result := p.topic.Publish(ctx, &gcppubsub.Message{Data: data})
+	if _, err := result.Get(ctx); err != nil {
+		return fmt.Errorf("publish build request: %w", err)
+	}
+
+	return nil
+}
+
+var invalidChars = regexp.MustCompile(`[^a-z0-9-]`)
+
+func (p *Publisher) ImageURI(projectName, commitSHA string) string {
+	name := strings.ToLower(projectName)
+	name = invalidChars.ReplaceAllString(name, "-")
+	name = strings.Trim(name, "-")
+	return fmt.Sprintf("%s/%s:%s", p.arRepo, name, commitSHA)
+}
+
+func (p *Publisher) Stop() {
+	p.topic.Stop()
+}
