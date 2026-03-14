ca78ac307c3659bdbdcbc292f9ec3b473531fdba
duolok <dusan.lecic5@gmail.com>
2026-03-14 18:41:29 +0100

    add: netword address translation
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

---
+++ infra/nat.tf
@@ -0,0 +1,13 @@
+resource "google_compute_router" "router" {
+  name    = "bifrost-router"
+  region  = var.region
+  network = google_compute_network.vpc.name
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "bifrost-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
