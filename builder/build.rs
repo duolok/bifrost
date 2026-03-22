5d747b94f7203e402baae2dd87cb5d6fc8af7145
duolok <dusan.lecic5@gmail.com>
2026-03-22 08:45:27 +0100

    update: build with correct path
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- builder/build.rs
+++ builder/build.rs
@@ -1,4 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-     tonic_build::compile_protos("../proto/events.proto")?;
-      Ok(())
+    // In Docker, proto is copied to ./proto/events.proto
+    // Locally, it's at ../proto/events.proto
+    let proto_path = if std::path::Path::new("proto/events.proto").exists() {
+        "proto/events.proto"
+    } else {
+        "../proto/events.proto"
+    };
+    tonic_build::compile_protos(proto_path)?;
+    Ok(())
 }
