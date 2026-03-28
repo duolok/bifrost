95d304659581b0ee0cb04c6532262d72c8e57217
duolok <dusan.lecic5@gmail.com>
2026-03-28 15:14:01 +0100

    add: gateway url config loading
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

---
+++ cli/src/config.rs
@@ -0,0 +1,12 @@
+use std::env;
+
+pub struct Config {
+    pub gateway_url: String,
+}
+
+pub fn load() -> Config {
+    Config {
+        gateway_url: env::var("BIFROST_GATEWAY_URL)
+            .unwrap_or_else(|_| "http://localhost:8080".into())
+    }
+}
