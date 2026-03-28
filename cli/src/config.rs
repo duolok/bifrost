ba5c03c9668e2b2526a48c4fb975c9e70e1faebe
duolok <dusan.lecic5@gmail.com>
2026-03-28 20:59:00 +0100

    add: realtime url for config
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -2,11 +2,14 @@ use std::env;
 
 pub struct Config {
     pub gateway_url: String,
+    pub realtime_url: String,
 }
 
 pub fn load() -> Config {
     Config {
         gateway_url: env::var("BIFROST_GATEWAY_URL")
             .unwrap_or_else(|_| "http://localhost:8080".into()),
+        realtime_url: env::var("BIFROST_REALTIME_URL")
+            .unwrap_or_else(|_| "http://localhost:4000".into()),
     }
 }
